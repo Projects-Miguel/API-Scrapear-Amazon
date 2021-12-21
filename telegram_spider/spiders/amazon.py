03707ec570404c4bee3ca0f062975aadc63ff025
Cbastiian <sebastiancordoba11999@gmail.com>
2021-12-21 15:07:03 -0500

    raspado de pagina de amazon con corrección de navegación de etiquetas

--- telegram_spider/spiders/amazon.py
+++ telegram_spider/spiders/amazon.py
@@ -3,20 +3,28 @@ import scrapy
 
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
-    search = 'televisores'
-    # allowed_domains = ['www.amazon.com.mx']
-    start_urls = ['https://www.amazon.com.mx/s?k={}'.format(search)]
+    start_urls = ['https://www.amazon.com.mx/s?k=']
 
     def parse(self, response):
-        return scrapy.Request(self.start_urls[0], self.after_init)
+        search = getattr(self, 'search_criteria', None)
+
+        if search is None or search == "":
+            raise Exception("The search criteria cant't be empty")
+        else:
+            splited_words = search.split(' ')
+            separator = '+'
+            parsed_word = separator.join(splited_words)
+            print(parsed_word)
+            return scrapy.Request(self.start_urls[0]+parsed_word, self.after_init)
 
     def after_init(self, response):
         result = []
         data = response.xpath(
-            '//*[@id = "search"]/div[1]/div[1]/div/span[3]/div[2]/div')[7:]
+            '//*[@id = "search"]/div[1]/div[1]/div/span[3]/div[2]/div')
 
         for item in data:
-            result.append("https://www.amazon.com.mx" +
-                          str(item.xpath(".//div/span/div/div/div[2]/div[1]/h2/a/@href").get()))
+            if item.xpath(".//div/div/div/div/div[2]/div[1]/h2/a/@href").get() is not None:
+                result.append("https://www.amazon.com.mx" +
+                              str(item.xpath(".//div/div/div/div/div[2]/div[1]/h2/a/@href").get()))
 
         yield {'items_urls': result[0:5]}
